dbb82568d42ca1ff835a9ca79111ee447ae4cca4
fxshunzzz <fxshunzzz@gmail.com>
2025-11-16 16:43:04 +0800

    init cris spring app

---
+++ src/main/java/org/taoyan/crisspringapp/CrisSpringAppApplication.java
@@ -0,0 +1,13 @@
+package org.taoyan.crisspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrisSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrisSpringAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/taoyan/crisspringapp/CrisSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.taoyan.crisspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrisSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
